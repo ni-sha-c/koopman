cd6f1a17f85dbb615341d2332afbe51cdf13e3b5
Nisha Chandramoorthy <nishac92@gmail.com>
2019-02-22 15:59:06 -0500

    Ulam for Arnold, generates correct eigenvalues, eigenvectors for Koopman.

--- src/arnoldUlam.py
+++ src/arnoldUlam.py
@@ -2,27 +2,25 @@ from numpy import *
 import sys
 from numba import jit
 from matplotlib.pyplot import *
+sys.path.insert(0,'../examples/')
+from arnoldcat import *
 
-@jit(nopython=True)
-def generate_ulam_matrix(n_nodes):
-    P = zeros((n_nodes, n_nodes))
-    for i in range(n_nodes):
-        P[2*i % n_nodes, i] = 1/2.
-        P[(2*i + 1) % n_nodes, i] = 1/2.
-    return P
 
-@jit(nopython=True)
+#@jit(nopython=True)
 def compute_eigenfunction(v, x):
     n_nodes = v.shape[0]
     x_grid = linspace(0, 1.,n_nodes+1)
     delta_x = x_grid[1] - x_grid[0]
     v_x = empty_like(x, dtype="complex")
-    for j,xj in enumerate(x):
-        for i in range(n_nodes + 1):
-            if(abs(xj - x_grid[i]) < delta_x):
-                node_x = i
-                break
-            v_x[j] = v[node_x]
+    n_points = x.shape[1]
+    n_dim = x.shape[0]
+    nodes_x = zeros((n_dim, n_points), dtype="int")
+    nodes = zeros(n_points, dtype="int")
+    n_nodes_per_dim = int(n_nodes**(1/n_dim))
+    for k in range(n_dim):
+        nodes_x[k] = build_chain(x[k],n_nodes_per_dim) 
+        nodes += nodes_x[k]*(n_nodes_per_dim**k)
+    v_x = v[nodes]
     return v_x
     
 @jit(nopython=True)
@@ -48,6 +46,16 @@ def solve_primal(solver, u0, s0, n=1):
     return u_trj
 
 @jit(nopython=True)
+def solve_multiple_init_cond(solver, u0, s0, n=1):
+    n_trj = u0.shape[0]
+    n_dim = u0.shape[1]
+    u_trj = zeros((n_trj, n_dim))
+    for i in range(n_trj):
+        u_trj[i] = solver.primal_step(\
+                u0[i], s0, n)
+    return u_trj
+
+@jit(nopython=True)
 def solve_tangent(solver, u_trj, v0):
     v_trj = empty_like(u_trj)
     v_trj[0] = v0
@@ -65,7 +73,42 @@ def observable(u):
     return f
 
 
-'''
+@jit(nopython=True)
+def build_transition_matrix(chain, n_nodes):
+    P = zeros((n_nodes,n_nodes))
+    chain_length = chain.shape[0]
+    for i in range(1,chain_length):
+        P[chain[i], chain[i-1]] += 1
+    for i in range(n_nodes):
+        p = sum(P[i])
+        if(p >= 1):
+            P[i] /= p
+    return P
+
+#@jit(nopython=True)
+def build_chain(x, n_nodes):
+    x_grid = linspace(0, 1.,n_nodes+1)
+    delta_x = x_grid[1] - x_grid[0]
+    n_points = x.shape[0]
+    chain_x = empty(n_points, dtype="int")
+    for j,xj in enumerate(x):
+        for i in range(n_nodes + 1):
+            if(abs(xj - x_grid[i]) < delta_x):
+                node_x = i
+                break
+        chain_x[j] = node_x
+    return chain_x
+
+@jit(nopython=True)
+def analytical_transition_matrix(P):
+    n_nodes = P.shape[0]
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            if(P[i,j] > 0.2):
+                P[i,j] = 0.25
+    return P
+
+
 solver = Solver()
 s0 = solver.s0
 s0[0] = 0.01
@@ -75,69 +118,73 @@ u_init = solver.primal_step(u_init, s0, \
         100)
 u_trj = solve_primal(solver, u_init, s0, \
         n_trj)
+u_trj = u_trj.T
+n_dim = solver.state_dim
+n_nodes_per_dim = 27
+chain_1d = empty((n_dim, n_trj), dtype='int')
+chain = zeros(n_trj, dtype='int')
+for i in range(n_dim):
+    chain_1d[i] = build_chain(u_trj[i], n_nodes_per_dim)
+    chain += chain_1d[i]*(n_nodes_per_dim**i)    
+n_nodes = n_nodes_per_dim**n_dim
+P = build_transition_matrix(chain, n_nodes)
+P = analytical_transition_matrix(P)
+l, V = eig(P.T)
 
-f_trj = observable(u_trj)
 
-f_min, f_max, f_sigma = min(f_trj), max(f_trj), \
-        std(f_trj)/sqrt(n_trj)
-
-n_nodes = 4
-f_grid = linspace(f_min - f_sigma, \
-        f_max + f_sigma, n_nodes + 1)
-f_delta = f_grid[1]-f_grid[0]
-chain = empty(n_trj, dtype='int')
-P = zeros((n_nodes, n_nodes))
-f0 = f_trj[0]
-
-for i, fi in enumerate(f_grid):
-    if(abs(f0 - fi) < f_delta):
-        chain[0] = i
-        break
-
-for n in range(1,n_trj):
-    fn = f_trj[n]
-    for i, fi in enumerate(f_grid):
-        if(abs(fn - fi) < f_delta):
-            chain[n] = i
-            P[chain[n],chain[n-1]] += 1.
-            break
-    
-for i in range(n_nodes):
-    p = sum(P[i])
-    P[i] /= p
-'''
-'''
-n_nodes = 35
-P = generate_ulam_matrix(n_nodes)
-l, V = eig(P)
 n_grid = 100
 x_grid = linspace(0.,1.-1.e-3,n_grid)
-eig_index = 5
+x_grid, y_grid = meshgrid(x_grid,x_grid)
+eig_index = 1
 vi = V[:,eig_index]
 f_grid = empty_like(x_grid,dtype="complex")
 Kf_grid = empty_like(x_grid,dtype="complex")
 Pf_grid = empty_like(x_grid,dtype="complex")
-for i in range(n_grid):
-    f_grid[i] = compute_eigenfunction(vi,x_grid[i])
-    Kf_grid[i] = compute_transfer_eigenfunction(vi,x_grid[i])
+u_grid = array([hstack(x_grid),hstack(y_grid)])
+f_grid = compute_eigenfunction(vi,u_grid)
+Kx_grid = solve_multiple_init_cond(solver, u_grid.T, s0, 1)
+Kf_grid = compute_eigenfunction(vi, Kx_grid.T)
+
+f_grid = reshape(f_grid, (n_grid, n_grid))
+Kf_grid = reshape(Kf_grid, (n_grid, n_grid))
 
 fig = figure(figsize=[15,10])
 ax = fig.add_subplot(121) 
 ax1 = fig.add_subplot(122)
-ax.plot(x_grid, real(l[eig_index]*f_grid), linewidth=3.0,label="$\Phi$")
-ax.plot(x_grid, real(Kf_grid), linewidth=3.0,label="$P\Phi$")
+im = ax.contourf(x_grid, y_grid, \
+        real(l[eig_index]*f_grid), label="$\Phi$")
+ax1.contourf(x_grid, y_grid,\
+        real(Kf_grid), linewidth=3.0,label="$K\Phi$")
 ax.set_xlabel("x",fontsize=24)
-#ax.set_title("Are these really eigenfunctions?",fontsize=24)
+ax.set_title("Real part of $\lambda \Psi$",fontsize=24)
 ax.tick_params(axis="both",labelsize=24)
-ax.legend()
-ax1.plot(x_grid, imag(l[eig_index]*f_grid), linewidth=3.0,label="$\Phi$")
-ax1.plot(x_grid, imag(Kf_grid), linewidth=3.0,label="$P\Phi$")
 ax1.set_xlabel("x",fontsize=24)
-#ax1.set_title("Are these really eigenfunctions?",fontsize=24)
+ax1.set_title("Real part of $K\Psi$",fontsize=24)
 ax1.tick_params(axis="both",labelsize=24)
-ax1.legend(fontsize=24)
-ax.legend(fontsize=24)
-savefig("../examples/plots/sawtoothUlam_transfer_eigenfunction_iterate.png")
+cax = fig.add_axes([0.9, 0.1, 0.01, 0.7])
+cb = colorbar(im, ax=[ax,ax1], cax=cax)
+cb.ax.yaxis.set_tick_params(labelsize=24)
+tight_layout(pad=2,rect=[0.01,0.01,0.9,0.9])
+savefig("../examples/plots/arnoldUlam_lefteigenvectors_P_real.png")
+
+fig = figure(figsize=[15,10])
+ax = fig.add_subplot(121) 
+ax1 = fig.add_subplot(122)
+ax.contourf(x_grid, y_grid, \
+        imag(l[eig_index]*f_grid))
+ax1.contourf(x_grid, y_grid,\
+        imag(Kf_grid))
+ax.set_xlabel("x",fontsize=24)
+ax.set_title("Imag part of $\lambda \Psi$",fontsize=24)
+ax.tick_params(axis="both",labelsize=24)
+ax1.set_xlabel("x",fontsize=24)
+ax1.set_title("Imag part of $K\Psi$",fontsize=24)
+ax1.tick_params(axis="both",labelsize=24)
+cax = fig.add_axes([0.9, 0.1, 0.01, 0.7])
+cb = colorbar(im, ax=[ax,ax1], cax=cax)
+cb.ax.yaxis.set_tick_params(labelsize=24)
+tight_layout(pad=2,rect=[0.01,0.01,0.9,0.9])
+savefig("../examples/plots/arnoldUlam_lefteigenvectors_P_imag.png")
 
 
 fig = figure(figsize=[15,10])
@@ -145,15 +192,63 @@ ax = fig.add_subplot(111)
 ax.plot(real(l), imag(l), "k.", ms = 15)
 ax.set_xlabel("Real part of eigenvalues of P",fontsize=20)
 ax.set_ylabel("Im part of eigenvalues of P",fontsize=20)
-ax.set_title("Frobenius-Perron spectra of the Sawtooth Map",fontsize=24)
+ax.set_title("Frobenius-Perron spectra of the Arnold's Cat Map",fontsize=20)
 ax.tick_params(axis="both",labelsize=24)
-r = 0.5
+A = array([2.,1.,1.,1.]).reshape(2,2)
+lA, VA = eig(A)
+r = min(lA)
 x = linspace(-r,r,50)
 y = sqrt(r*r - x*x)
-ax.plot(x, y, "k-.", alpha=0.5)
-ax.plot(x, -y, "k-.", alpha=0.5)
-savefig("../examples/plots/sawtoothUlam_eigenvalues.png")
+ax.plot(x, y, "k-.", linewidth=3.0,alpha=0.5)
+ax.plot(x, -y, "k-.", linewidth=3.0,alpha=0.5)
+tight_layout(pad=2,rect=[0.01,0.01,0.9,0.9])
+savefig("../examples/plots/arnoldUlam_eigenvalues.png")
 
+fig = figure(figsize=[15,10])
+ax = fig.add_subplot(121) 
+ax1 = fig.add_subplot(122)
+ax.contourf(x_grid, y_grid, \
+        real(l[eig_index]*f_grid))
+Kx_grid = solve_multiple_init_cond(solver, u_grid.T, s0, 3)
+Kf_grid = compute_eigenfunction(vi, Kx_grid.T)
+Kf_grid = reshape(Kf_grid, (n_grid, n_grid))
+ax1.contourf(x_grid, y_grid,\
+        real(Kf_grid))
+n_grid = 5
+eps = 1.e-1
+v_stable = VA[:,1]
+v_unstable = VA[:,0]
+x_grid = linspace(0.2,0.8,n_grid)
+x_grid, y_grid = meshgrid(x_grid,x_grid)
+x_grid = hstack(x_grid)
+y_grid = hstack(y_grid)
+x_grid_plus_vA = x_grid + eps*v_stable[0]
+y_grid_plus_vA = y_grid + eps*v_stable[1]
+ax1.plot([x_grid,x_grid_plus_vA],\
+        [y_grid,y_grid_plus_vA],\
+        linewidth=3.0, color='b', label="stable")
+x_grid_plus_vA = x_grid + eps*v_unstable[0]
+y_grid_plus_vA = y_grid + eps*v_unstable[1]
+ax1.plot([x_grid,x_grid_plus_vA],\
+        [y_grid,y_grid_plus_vA],\
+        linewidth=3.0, color='r', label="unstable")
+
+ax.set_xlabel("x",fontsize=24)
+ax.set_title("Real part of $\lambda \Psi$",fontsize=24)
+ax.tick_params(axis="both",labelsize=24)
+ax1.set_xlabel("x",fontsize=24)
+ax1.set_title("Real part of $K\Psi$",fontsize=24)
+ax1.tick_params(axis="both",labelsize=24)
+cax = fig.add_axes([0.9, 0.1, 0.01, 0.7])
+cb = colorbar(im, ax=[ax,ax1], cax=cax)
+cb.ax.yaxis.set_tick_params(labelsize=24)
+tight_layout(pad=2,rect=[0.01,0.01,0.9,0.9])
+savefig("../examples/plots/arnoldUlam_stable_unstable_directions.png")
+
+
+
+
+'''
 fig = figure(figsize=[15,10])
 ax = fig.add_subplot(121) 
 ax1 = fig.add_subplot(122)
